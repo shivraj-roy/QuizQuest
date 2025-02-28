13e1926a0d9dc6d87b9e14f5827fccc61778f605
Shivraj Roy <shivrajroy78@gmail.com>
2025-02-28 22:25:38 +0530

    add: Shuffled the answers & implement quiz completion status...

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,19 +1,31 @@
 import { useState } from "react";
 import QUESTIONS from "../../questions.js";
+import QuizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
    const [userAnswers, setUserAnswers] = useState([]);
 
    const activeQuestionIndex = userAnswers.length;
 
-   const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-   shuffledAnswers.sort(() => Math.random() - 0.5);
+   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
    const handleSelectedAns = (ans) => {
       setUserAnswers((prev) => {
          return [...prev, ans];
       });
    };
+
+   if (quizIsComplete) {
+      return (
+         <div id="summary">
+            <img src={QuizCompleteImg} alt="Quiz Completed..." />
+            <h2>Quiz is completed...</h2>
+         </div>
+      );
+   }
+
+   const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+   shuffledAnswers.sort(() => Math.random() - 0.5);
 
    return (
       <div id="quiz">
